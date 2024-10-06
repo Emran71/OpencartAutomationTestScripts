440844bb51ada3e12ea65fbb524d828d9cbb06db
emran71 <ekhan4475@gmail.com>
2024-10-06 12:47:59 +0600

    Test Log reports

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -3,19 +3,35 @@ package testBase;
 import java.time.Duration;
 
 import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 
 public class BaseClass {
 	
 	public WebDriver driver;
+	public Logger logger;
+	
 
 	@BeforeClass
-	public void setupBrowser()
-	{
-		driver= new ChromeDriver();
+	@Parameters({"OS","browser"})
+	public void setupBrowser(String os, String br)
+	{   
+		logger = LogManager.getLogger(this.getClass());
+		
+		
+		switch(br.toLowerCase()) {
+		case "chrome": driver= new ChromeDriver();break;
+		case "firefox": driver= new FirefoxDriver();break;
+		default: System.out.println("Invalid browser"); return;
+		}
+		
+		
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("https://tutorialsninja.com/demo/index.php?route=common/home");
--- src/test/java/testCases/TC_001_RegistrationTest.java
+++ src/test/java/testCases/TC_001_RegistrationTest.java
@@ -8,34 +8,50 @@ import pageObjects.RegistrationPage;
 import testBase.BaseClass;
 
 public class TC_001_RegistrationTest extends BaseClass{
-   
+
 	@Test
 	public void Reg_test()
-	{
+	{  
+		logger.info(".........Starting TC_001.......");
 		HomePage hp = new HomePage(driver);
 		hp.myAccount();
+		logger.info(".......Clicked On My account link......");
 		hp.registerMenu();
+		logger.info(".....Clicked On Register link........");
 
+		try
+		{
 		RegistrationPage regpage = new RegistrationPage(driver);
+
 		regpage.setFirstName(randomCharacterGenerate().toUpperCase());
+		logger.info(".........Inputed first name.......");
 		regpage.setLastName(randomCharacterGenerate().toUpperCase());
+		logger.info(".........Inputed second name.......");
 		regpage.setEmail(randomCharacterGenerate()+"@gmail.com");
+		logger.info(".........Inputed email name.......");
 		regpage.setPhone(randomNumberGenerate());
+		logger.info(".........Inputed Mobile Number.......");
 
 		String pass = randomAlphaNumaricGenerate();
 		regpage.setPassword(pass);
 		regpage.setConfirmPassword(pass);
-		
+		logger.info(".........Inputing Password.......");
+
 		regpage.clickSubscribe();
 		regpage.clickPrivacy();
 		regpage.clickContinue();
+		logger.info(".........Validating Expected Message.......");
 		String responsemgs = regpage.getConfirmationMessage();
 
-		Assert.assertEquals(responsemgs, "Your Account Has Been Created!");  	
+		Assert.assertEquals(responsemgs, "Your Account Has Been Created!");
+		} 
+		catch(Exception e)
+		{
+			logger.error("......Failed Test....");
+			logger.debug("....Debug logs.....");
+			Assert.fail();
+		}
+		logger.info(".........Finished TC_001.......");
 	}
-
-	
-	
-
 
 }
