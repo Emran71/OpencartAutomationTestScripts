bd98c67b6eb64626ee521f5388845a9600e1cf1c
emran71 <ekhan4475@gmail.com>
2024-10-13 22:52:13 +0600

    Grid System in selenium

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -3,6 +3,7 @@ package testBase;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Date;
@@ -12,10 +13,13 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
@@ -35,10 +39,42 @@ public class BaseClass {
 	p.load(file);
 
 	logger = LogManager.getLogger(this.getClass());
-	switch(br.toLowerCase()) {
-	case "chrome": driver= new ChromeDriver();break;
-	case "firefox": driver= new FirefoxDriver();break;
-	default: System.out.println("Invalid browser"); return;
+
+	if(p.getProperty("execution_env").equalsIgnoreCase("remote"))
+	{
+		DesiredCapabilities cap = new DesiredCapabilities();
+		if(os.equalsIgnoreCase("Windows"))
+		{
+			cap.setPlatform(Platform.WIN10);
+		}
+		else if(os.equalsIgnoreCase("mac"))
+		{
+			cap.setPlatform(Platform.MAC);
+		}
+		else 
+		{
+			System.out.println("No match OS");
+		}
+
+		switch(br.toLowerCase())
+		{
+		case "chrome": cap.setBrowserName("chrome");break;
+		case "firefox": cap.setBrowserName("firefox");break;
+		case "edge": cap.setBrowserName("edge");break;
+		default: System.out.println("Invalid browser"); return;
+		}
+		
+		driver= new RemoteWebDriver(new URL("http://192.168.56.1:4444/wd/hub"),cap);
+
+	}
+
+	if(p.getProperty("execution_env").equalsIgnoreCase("local"))
+	{
+		switch(br.toLowerCase()) {
+		case "chrome": driver= new ChromeDriver();break;
+		case "firefox": driver= new FirefoxDriver();break;
+		default: System.out.println("Invalid browser"); return;
+		}
 	}
 
 
@@ -74,16 +110,16 @@ public class BaseClass {
 		String alphanumeric = RandomStringUtils.randomAlphanumeric(10);
 		return alphanumeric;
 	}
-	
+
 	public String captureScreen (String tname) throws IOException 
 	{
 		String timeStamp = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
-		
+
 		TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
 		File sourceFile = takesScreenshot.getScreenshotAs (OutputType.FILE);
-		
+
 		String targetFilePath = System.getProperty("user.dir")+"\\screenshots\\" + tname + "_" + timeStamp + ".png" ;
-		
+
 		File targetFile=new File(targetFilePath);
 		sourceFile.renameTo(targetFile);
 		return targetFilePath;
